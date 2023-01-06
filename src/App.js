46287db7432ccc3c7d30b3031356c58de6fbcf7e
Jungki <andy960609@gmail.com>
2023-01-06 16:42:15 +0900

    counter 삭제 > slice 만 사용할거라서 Axios 설치 Axios GET,POST,DELETE 까지 수정

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
 // src/App.js
 
-import React from "react";
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addNumber, minusNumber } from "./redux/modules/counterSlice";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // axios import 합니다.
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const globalNumber = useSelector((state) => state.counter.number);
-  const dispatch = useDispatch();
+  const [todos, setTodos] = useState(null);
+  const [todo, setTodo] = useState({
+    title: "",
+  });
 
-  const onChangeHandler = (event) => {
-    const { value } = event.target;
-    setNumber(+value);
+  // axios를 통해서 get 요청을 하는 함수를 생성합니다.
+  // 비동기처리를 해야하므로 async/await 구문을 통해서 처리합니다.
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3001/todos");
+    setTodos(data); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
   };
-  const onClickAddNumberHandler = () => {
-    dispatch(addNumber(number));
+  const onSubmitHandler = async (todo) => {
+    await axios.post("http://localhost:3001/todos", todo);
+    setTodos([...todos.todo]);
   };
-  const onClickMinusNumberHandler = () => {
-    dispatch(minusNumber(number));
+
+  const onClickDeleteButtonHandler = (todoId) => {
+    axios.delete(`http://localhost:3001/todos/${todoId}`);
+    setTodos([...todos.todo]);
   };
-  console.log(number);
+  // 생성한 함수를 컴포넌트가 mount 됐을 떄 실행하기 위해 useEffect를 사용합니다.
+  useEffect(() => {
+    // effect 구문에 생성한 함수를 넣어 실행합니다.
+    fetchTodos();
+  }, [todos]);
+
+  // data fetching이 정상적으로 되었는지 콘솔을 통해 확인합니다.
+  console.log(todos); // App.js:16
   return (
-    <div>
-      {globalNumber}
-      <input type="number" onChange={onChangeHandler} />
-      <button onClick={onClickAddNumberHandler}>더하기</button>
-      <button onClick={onClickMinusNumberHandler}>빼기</button>
-    </div>
+    <>
+      <form
+        onSubmit={(e) => {
+          // 👇 submit했을 때 브라우저의 새로고침을 방지합니다.
+          e.preventDefault();
+          onSubmitHandler(todo);
+        }}
+      >
+        <input
+          type="text"
+          onChange={(ev) => {
+            const { value } = ev.target;
+            setTodo({
+              ...todo,
+              title: value,
+            });
+          }}
+        />
+        <button>추가하기</button>
+      </form>
+      <div>
+        {" "}
+        {todos?.map((todo) => (
+          <div key={todo.id}>
+            {todo.title}
+            <button
+              type="button"
+              onClick={() => onClickDeleteButtonHandler(todo.id)}
+            >
+              삭제하기
+            </button>
+          </div>
+        ))}{" "}
+      </div>{" "}
+    </>
   );
 };
 
